292b10409fb8abed90308106b74fb5d2f2103c35
Colin Coltrera <c.coltrera@gmail.com>
2015-07-07 09:24:17 -0700

    Adapt greetMod to check if argument an array, and take the property at index 2. Add test to ensure input processed correctly. Change greet.js to work with refactor.

--- test/greetSpec.js
+++ test/greetSpec.js
@@ -1,8 +1,18 @@
 var expect = require("chai").expect;
 var greet = require("../lib/greetMod.js").greet;
 
+
 describe("greet", function() {
   it("should take a name and return 'hello ' + name", function() {
     expect(greet("Colin")).to.deep.equal("hello Colin");
-  })
-})
+  });
+  it("should tell if input is an array, and take the member at index 2", function() {
+    expect(greet([
+      'node',
+      '/Users/colincoltrera/projects/codefellows/sea-d41/greet-app/greet',
+      'Colin'
+    ])).to.deep.equal("hello Colin");
+  });
+});
+
+
