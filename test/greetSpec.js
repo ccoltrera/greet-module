1afd3788ab20a99e3ba55f80bf4e864e684d2854
Colin Coltrera <c.coltrera@gmail.com>
2015-07-07 17:42:29 -0700

    Add test that actually executes command line based test.

--- test/greetSpec.js
+++ test/greetSpec.js
@@ -1,17 +1,24 @@
 var expect = require("chai").expect;
+var exec = require("child_process").exec;
 var greet = require("../lib/greetMod.js").greet;
 
 
 describe("greet", function() {
+  var captured_stdout;
+  before(function(done) {
+    exec('node ./greet "Colin"', function(error, stdout, stderr) {
+        if (error) {
+          done (error);
+        }
+        captured_stdout = stdout.trim();
+        done();
+    });
+  });
   it("should take a name and return 'hello ' + name", function() {
     expect(greet("Colin")).to.deep.equal("hello Colin");
   });
-  it("should tell if input is an array, and take the member at index 2", function() {
-    expect(greet([
-      'node',
-      '/Users/colincoltrera/projects/codefellows/sea-d41/greet-app/greet',
-      'Colin'
-    ])).to.deep.equal("hello Colin");
+  it("should be usable as a command line tool, taking a process.argv", function() {
+    expect(captured_stdout).to.equal("hello Colin");
   });
 });
 
