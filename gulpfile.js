337f6e5577cd4f752ee2f6284459ed17e5f46404
Colin Coltrera <c.coltrera@gmail.com>
2015-07-08 11:06:47 -0700

    Add .jshintrc file to root, with node and globals. Add gulp.watch to check for changes. Make 'lint' task more specific in src.

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require("gulp");
+var mocha = require("gulp-mocha");
+var jshint = require("gulp-jshint");
+
+gulp.task("default", ["test", "lint"], function() {});
+
+gulp.task("test", function() {
+  return gulp.src("test/*Spec.js")
+              .pipe(mocha());
+});
+
+gulp.task("lint", function() {
+  return gulp.src(["./*.js","./test/*.js","./lib/*.js"])
+              .pipe(jshint())
+              .pipe(jshint.reporter("default"));
+});
+
+var watcher = gulp.watch(["./*.js","./test/*.js","./lib/*.js"], ["test", "lint"]);
